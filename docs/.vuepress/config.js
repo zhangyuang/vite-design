195f6077410857b6f8852ba54d4cb574fb69f1a4
zhangyuang <leonin0829@gmail.com>
2021-04-03 17:13:01 +0800

    docs: add ssr.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,7 +54,8 @@ module.exports = {
             'optimize',
             'render',
             'hmr',
-            '2.0/optimize'
+            '2.0/optimize',
+            '2.0/ssr'
           ],
         }
       ]
